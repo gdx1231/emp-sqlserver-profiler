f9a011c65ab25f3c64c5ada7fabbb8f3dd251f23
Code guo <guolei@gdxsoft.com>
2022-06-26 12:57:35 +0800

    s

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
-#!/bin/sh
-
+#!/bin/bash
 sh_dir=`dirname $0`
 echo $sh_dir
-java -cp $sh_dir/target/emp-sqlserver-profiler-1.0.0.jar:$sh_dir/target/lib/* com.gdxsoft.sqlProfiler.ProfilerControl $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 
-
+java -cp $sh_dir/target/emp-sqlserver-profiler-1.0.0.jar:$sh_dir/target/lib/* \
+	com.gdxsoft.sqlProfiler.ProfilerControl $1 $2 $3 $4 $5 $6 $7 $8 $9 $10 
